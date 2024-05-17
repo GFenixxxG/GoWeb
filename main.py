f3da059542f84e5ad01fcb917ed1649f12526374
Fenixxx <ehorpython@gmail.com>
2024-05-17 20:12:25 +0300

    V1

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-from flask import Flask, url_for, render_template, redirect
+from flask import Flask, url_for, render_template, redirect, session
+
+def startSession(quiz_id = 0):
+    session['quiz'] = quiz_id
+    session['last_question'] = 0
+    session["right_ans"] = 0
+    session["wrong_ans"] = 0
+    session["total"] = 0
+
+
 
 app = Flask(__name__)
 
@@ -19,6 +28,8 @@ def result():
 @app.route("/add_quiz")
 def add_quiz():
     return "<h1>add_quiz Page</h1>"
+
+app.config["SECRET_KEY"] = '00000000'
 
 if __name__ == "__main__":
     app.run()
